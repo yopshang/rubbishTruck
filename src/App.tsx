9a722e13f73efd54585c1253bedf8bdf154de44d
EZPAY_OA\shang.huang <shang.huang@ezpay.com.tw>
2023-07-31 15:21:05 +0800

    feature:  use env

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import MainNavbar from './components/navBar'
+import Googlemap from './components/GoogleMap';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from 'react'
@@ -12,26 +13,7 @@ function App() {
   return (
     <>
       <MainNavbar></MainNavbar>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <Googlemap></Googlemap>
     </>
   )
 }
---
+++ src/components/GoogleMap.tsx
@@ -0,0 +1,11 @@
+import {GoogleMapReact} from "google-map-react"
+function Googlemap(){
+  const reactkey = import.meta.env.VITE_APP_KEY;
+  return (
+    <>test
+      {reactkey}
+    </>
+  )
+}
+
+export default Googlemap
